6a97c88ce8b0bf079a932139496a5c19bfd05ebc
Semen Ishkhanyan <semien9797@gmail.com>
2018-04-06 03:30:49 +0300

    update Factories

--- Factories.cpp
+++ Factories.cpp
@@ -4,6 +4,14 @@ Technique* TankFactory::createTechnique(const Point& initialPosition, sf::Render
     return new Tank(initialPosition, mainWindow, isEnemy);
 }
 
+Technique* CarFactory::createTechnique(const Point& initialPosition, sf::RenderWindow* mainWindow, bool isEnemy){
+    return new Car(initialPosition, mainWindow);
+}
+
 Tower* SimpleTowerFactory::createTower(const Point& initialPosition, sf::RenderWindow* mainWindow){
     return new SimpleTower(initialPosition, mainWindow);
+}
+
+Tower* ElectricTowerFactory::createTower(const Point& initialPosition, sf::RenderWindow* mainWindow){
+    return new ElectricTower(initialPosition, mainWindow);
 }
--- Factories.h
+++ Factories.h
@@ -16,7 +16,15 @@ public:
     Technique* createTechnique(const Point& initialPosition, sf::RenderWindow* mainWindow, bool isEnemy);
 };
 
+class CarFactory : public TechniqueFactory{
+    Technique* createTechnique(const Point& initialPosition, sf::RenderWindow* mainWindow, bool isEnemy);
+};
+
 class SimpleTowerFactory : TowerFactory {
 public:
+    Tower* createTower(const Point& initialPosition, sf::RenderWindow* mainWindow);
+};
+
+class ElectricTowerFactory : TowerFactory{
     Tower* createTower(const Point& initialPosition, sf::RenderWindow* mainWindow);
 };
